f897349150ad1f5ca62fdc5111e4390ff2497656
Benjamin Besse <benjamin.besse02@gmail.com>
2014-09-11 10:55:39 +0200

    + start implementing dropdown select

--- js/libs/materialbootstrap/dropdown.js
+++ js/libs/materialbootstrap/dropdown.js
@@ -7,7 +7,7 @@
     var backdrop = '.dropdown-backdrop';
     var toggle   = '[data-toggle="dropdown"]';
     var Dropdown = function (element) {
-        $(element).on('click.bs.dropdown', this.toggle)
+        $(element).on('click.bs.dropdown', this.toggle);
     };
 
     Dropdown.VERSION = '1.0.0';
@@ -23,11 +23,27 @@
 
         clearMenus();
 
+        if($ul.attr('role') == 'select') {
+            initSelectMenu({
+                $ul : $ul,
+                $button : $this,
+                $parent : $parent
+            });
+        }
+
         if (!isActive) {
             if ('ontouchstart' in document.documentElement && !$parent.closest('.navbar-nav').length) {
                 // if mobile we use a backdrop because click events don't delegate
                 $('<div class="dropdown-backdrop"/>').insertAfter($(this)).on('click', clearMenus)
             }
+            $ul.find('li a').on('click', function(evt) {
+                $parent.trigger('select.bs.dropdown', evt);
+                selectItem(evt, {
+                    $this   : $this,
+                    $ul     : $ul,
+                    $parent : $parent
+                });
+            });
 
             var relatedTarget = { relatedTarget: this };
             $parent.trigger(e = $.Event('show.bs.dropdown', relatedTarget));
@@ -44,6 +60,8 @@
                     .toggleClass('open')
                     .trigger('shown.bs.dropdown', relatedTarget)
             });
+        } else {
+            $ul.find('li a').off('click');
         }
 
         return false
@@ -100,6 +118,7 @@
         })
     }
 
+
     function getParent($this) {
         var selector = $this.attr('data-target');
 
@@ -152,6 +171,36 @@
         }
     };
 
+    function initSelectMenu(params) {
+        if (params.$ul.find('li.header').length === 0) {
+            // add header selected
+            var $liSelected = $('<li class="header"><a role="menuitem" href="#">'+ params.$button.find('.name').html() +'</a></li>');
+            params.$ul.prepend('<li role="presentation" class="divider"></li>');
+            params.$ul.prepend($liSelected);
+        }
+        if (params.$parent.find('input[type="hidden"]').length == 0) {
+            params.$parent.append('<input type="hidden" name="'+ (params.$ul.data('name') || '') +'" />');
+        }
+    }
+
+    function selectItem(evt, params) {
+        console.log('selectItem');
+        var $a = $(evt.currentTarget),
+            $this = params.$this,
+            $ul = params.$ul,
+            $parent = params.$parent,
+            $input = $parent.find('input[type="hidden"]');
+
+        if ($ul.attr('role') != 'select') return;
+        if ($a.hasClass('disabled')) return;
+
+        evt.preventDefault();
+
+        $a.addClass('selected');
+        $input.val($a.data('value'));
+        $this.find('.name').html($a.html());
+    }
+
 
     // DROPDOWN PLUGIN DEFINITION
     // ==========================
@@ -188,7 +237,7 @@
         .on('click.bs.dropdown.data-api', clearMenus)
         .on('click.bs.dropdown.data-api', '.dropdown form', function (e) { e.stopPropagation() })
         .on('click.bs.dropdown.data-api', toggle, Dropdown.prototype.toggle)
-        .on('keydown.bs.dropdown.data-api', toggle + ', [role="menu"], [role="listbox"]', Dropdown.prototype.keydown)
+        .on('keydown.bs.dropdown.data-api', toggle + ', [role="menu"], [role="listbox"], [role="select"]', Dropdown.prototype.keydown)
 
 
 })(jQuery);
